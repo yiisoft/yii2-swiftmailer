23b69e5c257c30bfcb1f88157c135a979f22d8eb
Klimov Paul <klimov.paul@gmail.com>
2017-04-12 18:12:45 +0300

    Compatibility with PHPUnit 6.x fix

--- tests/compatibility.php
+++ tests/compatibility.php
@@ -3,14 +3,14 @@
  * Ensures compatibility with PHPUnit < 6.x
  */
 
-if (!class_exists('PHPUnit\Framework\Constraint\Constraint') && class_exists('PHPUnit_Framework_Constraint')) {
-    namespace PHPUnit\Framework\Constraint {
+namespace PHPUnit\Framework\Constraint {
+    if (!class_exists('PHPUnit\Framework\Constraint\Constraint') && class_exists('PHPUnit_Framework_Constraint')) {
         abstract class Constraint extends \PHPUnit_Framework_Constraint {}
     }
 }
 
-if (!class_exists('PHPUnit\Framework\TestCase') && class_exists('PHPUnit_Framework_TestCase')) {
-    namespace PHPUnit\Framework {
+namespace PHPUnit\Framework {
+    if (!class_exists('PHPUnit\Framework\TestCase') && class_exists('PHPUnit_Framework_TestCase')) {
         abstract class TestCase extends \PHPUnit_Framework_TestCase {}
     }
 }
